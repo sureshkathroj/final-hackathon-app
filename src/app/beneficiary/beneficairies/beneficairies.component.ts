c2e151c788784c396f4b0ff0cb2f845ab7af8562
suresh <sureshkathroj@gmail.com>
2021-03-11 14:45:56 +0530

    summary added

--- src/app/beneficiary/beneficairies/beneficairies.component.ts
+++ src/app/beneficiary/beneficairies/beneficairies.component.ts
@@ -1,66 +1,68 @@
+import { RestApiService } from './../../helpers/rest-api.service';
 import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 
-export interface Card {
-  title: string;
-  subtitle: string;
-  text: string;
+export interface Favourities {
+  id: number;
+  accountnumber: string;
+  name: string;
+  bank: string;
 }
 
-const DATA: Card[] = [
-  {
-    title: 'Suresh',
-    subtitle: '12365478954789',
-    text: 'ING bank'
-  },
-  {
-    title: 'Suresh',
-    subtitle: '12365478954789',
-    text: 'ING bank'
-  },
-  {
-     title: 'Suresh',
-    subtitle: '12365478954789',
-    text: 'ING bank'
-  },
-  {
-     title: 'Suresh',
-    subtitle: '12365478954789',
-    text: 'ING bank'
-  },
-  {
-    title: 'Suresh',
-    subtitle: '12365478954789',
-    text: 'ING bank'
-  },
-  {
-    title: 'Shiba Inu 6',
-    subtitle: 'Dog Breed',
-    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
-  },
-  {
-   title: 'Suresh',
-    subtitle: '12365478954789',
-    text: 'ING bank'
-  },
-  {
-    title: 'Shiba Inu 8',
-    subtitle: 'Dog Breed',
-    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
-  },
-  {
-    title: 'Shiba Inu 9',
-    subtitle: 'Dog Breed',
-    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
-  },
-  {
-    title: 'Shiba Inu 10',
-    subtitle: 'Dog Breed',
-    text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
-  }
-];
+// const DATA: Card[] = [
+//   {
+//     title: 'Suresh',
+//     subtitle: '12365478954789',
+//     text: 'ING bank'
+//   },
+//   {
+//     title: 'Suresh',
+//     subtitle: '12365478954789',
+//     text: 'ING bank'
+//   },
+//   {
+//      title: 'Suresh',
+//     subtitle: '12365478954789',
+//     text: 'ING bank'
+//   },
+//   {
+//      title: 'Suresh',
+//     subtitle: '12365478954789',
+//     text: 'ING bank'
+//   },
+//   {
+//     title: 'Suresh',
+//     subtitle: '12365478954789',
+//     text: 'ING bank'
+//   },
+//   {
+//     title: 'Shiba Inu 6',
+//     subtitle: 'Dog Breed',
+//     text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+//   },
+//   {
+//    title: 'Suresh',
+//     subtitle: '12365478954789',
+//     text: 'ING bank'
+//   },
+//   {
+//     title: 'Shiba Inu 8',
+//     subtitle: 'Dog Breed',
+//     text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+//   },
+//   {
+//     title: 'Shiba Inu 9',
+//     subtitle: 'Dog Breed',
+//     text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+//   },
+//   {
+//     title: 'Shiba Inu 10',
+//     subtitle: 'Dog Breed',
+//     text: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.'
+//   }
+// ];
 
 @Component({
   selector: 'app-beneficairies',
@@ -72,20 +74,35 @@ export class BeneficairiesComponent implements OnInit, OnDestroy {
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   obs: Observable<any> | undefined;
-  dataSource: MatTableDataSource<Card> = new MatTableDataSource<Card>(DATA);
-  constructor(private changeDetectorRef: ChangeDetectorRef) { }
+  ELEMENT_DATA: Favourities[] = [];
+  dataSource: MatTableDataSource<Favourities> = new MatTableDataSource<Favourities>(this.ELEMENT_DATA);
+  constructor(private changeDetectorRef: ChangeDetectorRef, private _service:RestApiService) { }
 
   ngOnInit(): void {
-    this.changeDetectorRef.detectChanges();
-    this.dataSource.paginator = this.paginator;
-    this.obs = this.dataSource.connect();
-
+    // this.changeDetectorRef.detectChanges();
+    // this.dataSource.paginator = this.paginator;
+    // this.obs = this.dataSource.connect();
+    this.getFavourities();
   }
 
   ngOnDestroy() {
     if (this.dataSource) {
       this.dataSource.disconnect();
     }
+  }
+
+  getFavourities () {
+    this._service.getFavourites().subscribe((data:any) => {
+
+      this.dataSource.data = data;
+
+      this.dataSource.paginator = this.paginator;
+
+      this.changeDetectorRef.detectChanges();
+      this.dataSource.paginator = this.paginator;
+      this.obs = this.dataSource.connect();
+      return data;
+    });
   }
 
 }
--- src/app/helpers/rest-api.service.ts
+++ src/app/helpers/rest-api.service.ts
@@ -12,7 +12,11 @@ export class RestApiService {
   constructor(
     protected httpClient: HttpClient
   ) { }
+    private baseURL = "http://localhost:3000/";
 
+    getFavourites () {
+      return this.httpClient.get(`${this.baseURL}beneficiaries`)
+    }
   protected getMethod(endPointUrl: string, baseParam: Action, httpOptions: {}): Observable<any> {
     return this.httpClient.get(`${endPointUrl}${baseParam.url}`, { ...httpOptions })
       .pipe(map(res => this.handleResponse(res)), catchError(error => this.handleHttpError('get', error)));
