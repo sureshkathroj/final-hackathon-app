3b2e23e7663aaa2411e7dc9bd441d5e468ce13cd
swapnil.kamble <swapnil.kamble@us.imshealth.com>
2021-03-11 11:29:11 +0530

    Routing

--- src/app/beneficiary/beneficiary-routing.module.ts
+++ src/app/beneficiary/beneficiary-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
       }, {
         path: 'edit',
         component: EditBenficairiesComponent
+      },
+      {
+        path: '',
+        component: SummaryBeneficiaryComponent
       }
     ]
   }];
